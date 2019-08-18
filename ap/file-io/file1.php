7404a1ae2c40b623f7b2c6281bd9c21d16f5fc5f
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-18 02:18:04 -0700

    file mode prac

--- ap/file-io/file1.php
+++ ap/file-io/file1.php
@@ -8,7 +8,7 @@
 
 
 //pracReadFile1();
-pracWriteFile();
+//pracWriteFile();
 
 /**
  * fopen(), fread(), feof()
@@ -27,7 +27,7 @@ function pracReadFile1 () {
  * file_put_contents(), fopen(), fgetcsv()
  */
 function pracWriteFile () {
-    $filename = 'prac.csv';
+    $filename = 'prac2.csv';
     $dataStr = '1,2,3,4,5';
     file_put_contents('./rf/'.$filename, $dataStr);
     $handle = fopen("./rf/$filename", 'r');
@@ -35,6 +35,30 @@ function pracWriteFile () {
     echo gettype($result) . ', ';
     echo count($result);
 }
+
+//prac1();
+function prac1 () {
+    file_put_contents('./rf/data.csv', '1,2,3,4,5');
+    $handle = fopen('./rf/data.csv', 'c+');
+    $data = fgetcsv($handle, 0);
+    var_dump($data[1]);
+}
+
+prac2();
+function prac2 () {
+    $f = './rf/data2.csv';
+    file_put_contents($f, '1,2,3,4,5');
+    $handle = fopen($f, 'c');
+    fputcsv($handle, ['6','7','8']);
+    fclose($handle);
+    echo file_get_contents($f);
+}
+
+
+
+
+
+
 
 
 
