73a9fdc684d4b6a7660f6415f2fa1756683214b6
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-17 17:19:35 -0700

    open file, read file, write to file

--- ap/file-io/file1.php
+++ ap/file-io/file1.php
@@ -7,16 +7,33 @@
  */
 
 
-pracReadFile1();
+//pracReadFile1();
+pracWriteFile();
 
+/**
+ * fopen(), fread(), feof()
+ */
 function pracReadFile1 () {
-    $handle = fopen('./file.txt', 'r');
+    $handle = fopen('./rf/ok-list.txt', 'r');
     while(!feof($handle)) {
         $v = fread($handle, 1024);
         echo "file value = $v";
         $break = 'point';
     }
     fclose($handle);
+}
+
+/**
+ * file_put_contents(), fopen(), fgetcsv()
+ */
+function pracWriteFile () {
+    $filename = 'prac.csv';
+    $dataStr = '1,2,3,4,5';
+    file_put_contents('./rf/'.$filename, $dataStr);
+    $handle = fopen("./rf/$filename", 'r');
+    $result = fgetcsv($handle);
+    echo gettype($result) . ', ';
+    echo count($result);
 }
 
 
