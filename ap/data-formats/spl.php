a5a724aac4ebfbb8fa99cfb1bbbcf9e936fd319d
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-19 02:26:41 -0700

    WOW 😲I discovered it took PHP about 60secs to scan
    647,852 records while writing the advancedFixedArray() function, it's not using a SplFixedArray class at the moment, but I didn't know PHP could scan that many records so fast.

--- ap/data-formats/spl.php
+++ ap/data-formats/spl.php
@@ -6,26 +6,58 @@
  * Time: 1:03 AM
  */
 
-$fxArray = new SplFixedArray(5);
 
-$fxArray[1] = 2;
-$fxArray[4] = 'hi';
+advancedFixedArrayPrac();
 
-foreach ($fxArray as $item) {
-    echo "\n$item\n";
+function advancedFixedArrayPrac() {
+    $handle = fopen('H1B2016.csv', 'r');
+    $headerRow = fgetcsv($handle, 0, ";");
+    $fieldCount = count($headerRow);
+    $count = 0;
+    $row = null;
+    $countF = null;
+    //$fxArray = new SplFixedArray($fieldCount);
+
+    while (!feof($handle)) {
+        $row = fgetcsv($handle, 0, ";");
+        $caseNumber = $row[1];
+        $caseStatus = $row[2];
+
+        if($count % 1000 === 0) {
+            $countF = number_format($count);
+            echo "\nrow $countF = case $caseNumber was $caseStatus\n";
+        }
+
+        $count++;
+    }
+
+    $totalCounts = number_format($count);
+    echo "\n\nProgram Complete, scanned $totalCounts records\n\n";
+    //$break = 'point';
+    fclose($handle);
 }
 
-e();
 
-for($i = 0; $i < count($fxArray); $i++) {
-    $elem = $fxArray[$i];
-    echo "\n$i - $elem\n";
+function basicFixedArrayPrac() {
+    $fxArray = new SplFixedArray(5);
+
+    $fxArray[1] = 2;
+    $fxArray[4] = 'hi';
+
+    foreach ($fxArray as $item) {
+        echo "\n$item\n";
+    }
+
+    e();
+
+    for ($i = 0; $i < count($fxArray); $i++) {
+        $elem = $fxArray[$i];
+        echo "\n$i - $elem\n";
+    }
 }
 
 
-
-
-
+//-- Script Utility Functions:
 
 function e() {
     echo "\n\n----\n\n";
