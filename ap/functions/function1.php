a1fd165969bc7bb45c885086a54c11ad523fb5fa
Julius Alvarado <javascript.uiux@gmail.com>
2019-08-18 23:30:15 -0700

    a bit of function prac

--- ap/functions/function1.php
+++ ap/functions/function1.php
@@ -5,3 +5,32 @@
  * Date: 8/17/2019
  * Time: 3:34 PM
  */
+
+function e() {
+    echo "\n\n----\n\n";
+}
+
+e();
+
+//practiceOne();
+function practiceOne() {
+    // try to fully grok variable functions
+    $lambdaOne = function (int $x): string {
+        return ($x * 0.5) . ' to ' . ($x * 1.5);
+    };
+
+    echo gettype($lambdaOne), ' | type $lambdaOne(2400) = ' . gettype($lambdaOne(2400));
+    echo "\n\n lambdaOne class = " . get_class($lambdaOne);
+    echo "\n\n hours to learn = " . $lambdaOne(2400);
+}
+
+e();
+
+$someNum = fatalErrorPrac(2,4);
+echo "some num = $someNum ", gettype($someNum);
+
+function fatalErrorPrac(?float $x, ?float $y): float {
+   return (double)$x * (double)$y;
+}
+
+$break = 'point';
