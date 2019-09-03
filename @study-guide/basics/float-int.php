c3f70504842de94cc5c5cd466940070e44bba420
Julius Alvarado <javascript.uiux@gmail.com>
2019-09-02 18:11:49 -0700

    renamed ap to @study-guide, practiced casting from
    float to int a bit and learned a bit about how PHP's internal representation of floating point numbers can cause counter intuitive results due to some floats being rational in radix 10 but irrational in radix 2

---
+++ @study-guide/basics/float-int.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/2/2019
+ * Time: 6:00 PM
+ */
+
+$x1 = (int)((0.1 + 0.7) * 10.0); // 0.7
+$y1 = (int)((0.1 + 0.5) * 10.0); //0.6
+
+$x2 = ((0.1 + 0.7) * 10.0); // 8.0
+$x2a = (int)$x2; // this will still output 7... damn.
+$x2b = (int)((string)$x2); // this fixes it, now it's 8
+$y2 = ((0.1 + 0.5) * 10.0);
+$y2 = (int)$y2;
+
+echo "\n\nx1 = $x1, y1 = $y1\n\n";
+echo "\n\nx2a = $x2a, x2b = $x2b, y2 = $y2\n\n";
+
+
+//
