10a62825b633434ed0e529bf837c352117b18f4f
Julius Alvarado <javascript.uiux@gmail.com>
2019-09-22 23:27:11 -0700

    a bit of list construct prac

--- @study-guide/arrays/array1.php
+++ @study-guide/arrays/array1.php
@@ -5,3 +5,39 @@
  * Date: 8/17/2019
  * Time: 3:34 PM
  */
+
+
+$jobData = ['pressure seal', 'converted', '1/0'];
+
+// list construct prac
+[$jobType, $paper, $color] = $jobData;
+/*
+//-- old way:
+$jobType = $jobData[0];
+$paper = $jobData[1];
+$color = $jobData[2];
+*/
+
+echo "\n\nThe $jobType job is a $paper $color \n\n";
+
+$info = [];
+$infoMap = [];
+
+[$info[0], $info[1], $info[2]] = $jobData;
+[$infoMap['job_type'], $infoMap['paper'], $infoMap['color']] = $jobData;
+
+$formattedVar = print_r($info, true);
+$formattedVarMap = print_r($info, true);
+
+echo $formattedVar;
+echo "\n\n";
+echo $formattedVarMap;
+
+
+$break = 'point';
+
+
+
+
+
+//
