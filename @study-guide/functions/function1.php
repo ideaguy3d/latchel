8117947cabfac8f96e1e1e68a6f50307c7c9819a
Julius Alvarado <javascript.uiux@gmail.com>
2019-09-22 23:00:34 -0700

    a bit of variadic practice

--- @study-guide/functions/function1.php
+++ @study-guide/functions/function1.php
@@ -30,15 +30,47 @@ e();
 //echo "some num = $someNum ", gettype($someNum);
 
 function fatalErrorPrac(?float $x, ?float $y): float {
-   return (double)$x * (double)$y;
+    return (double)$x * (double)$y;
 }
 
-if(!is_callable(function () {echo 'hello there';})) {
+$someF = function () {
+    echo 'hello there';
+};
+
+if (!is_callable($someF)) {
     function sayHello() {
         echo "Hello ^_^/";
     }
 }
+else {
+    function sayHello() {
+        echo "I was never defined :'(";
+    }
+}
 
+// sayHello only gets defined if a condition is met
 sayHello();
+
+paramTypes(1);
+paramTypes(1, 2);
+paramTypes(1, 2, 3);
+paramTypes(1, 2, 3, 4);
+paramTypes(1, 2, 3, 4, 5);
+
+// variadic prac
+function paramTypes(int $req, int $opt = null, ...$vari): void {
+    echo "\n";
+    printf(
+        'Required: %d, Optional: %d, Variadic length: %d' . "\n",
+        $req, $opt, count($vari)
+    );
+
+    foreach ($vari as $v) {
+        echo "$v\n";
+    }
+
+}
+
+
 
 $break = 'point';
--- @study-guide/strings/strings1.php
+++ @study-guide/strings/strings1.php
@@ -5,3 +5,15 @@
  * Date: 8/17/2019
  * Time: 3:35 PM
  */
+
+
+// encoding / multi byte / char output prac
+
+
+
+
+
+
+
+
+// end of file
