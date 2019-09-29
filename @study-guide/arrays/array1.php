eb0f83c88397f5bcb6e39db9febdf5962bccb7bb
Julius Alvarado <javascript.uiux@gmail.com>
2019-09-29 14:36:21 -0700

    a bit of array prac

--- @study-guide/arrays/array1.php
+++ @study-guide/arrays/array1.php
@@ -6,19 +6,46 @@
  * Time: 3:34 PM
  */
 
+//-- diff prac --\\
+$mandatoryKeys = ['email', 'password', 'token', 'username'];
+$mockPostKeys = ['email', 'password',];
+$mockPostKeys2 = ['username', 'password', 'email'];
 
-$jobData = ['pressure seal', 'converted', '1/0'];
+$diff = array_diff($mandatoryKeys, $mockPostKeys, $mockPostKeys2);
+
+$break = 'point';
+
+//-- union prac --\\
+$jobData = [
+    ['job_type', 'seal_type', 'color'],
+    ['pressure seal', 'converted', '1/0'],
+    ['post card', 'n/a', '1/4'],
+    ['self mailer', 'n/a', '4/0']
+];
+
+$jobData2 = [
+    ['job_type', 'seal_type', 'color'],
+    ['letter', 'n/a', '1/0'],
+    ['letter', 'n/a', '1/0'],
+    ['flat', 'n/a', '1/0']
+];
+
+$jobRec = ['pressure seal', 'converted', '1/0'];
+
+//$jobDataUnion = $jobData + $jobData2;
+$jobDataUnion2 = array_merge($jobData, array_slice($jobData2, 1));
 
 // list construct prac
 [$jobType, $paper, $color] = $jobData;
+
 /*
-//-- old way:
+// -- old way --
 $jobType = $jobData[0];
 $paper = $jobData[1];
 $color = $jobData[2];
 */
 
-echo "\n\nThe $jobType job is a $paper $color \n\n";
+//echo "\n\nThe $jobType job is a $paper $color \n\n";
 
 $info = [];
 $infoMap = [];
@@ -27,11 +54,13 @@ $infoMap = [];
 [$infoMap['job_type'], $infoMap['paper'], $infoMap['color']] = $jobData;
 
 $formattedVar = print_r($info, true);
-$formattedVarMap = print_r($info, true);
+$formattedVarMap = print_r($infoMap, true);
 
+/*
 echo $formattedVar;
 echo "\n\n";
 echo $formattedVarMap;
+*/
 
 
 $break = 'point';
