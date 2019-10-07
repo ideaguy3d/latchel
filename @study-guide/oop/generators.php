fe6073a515de9eda1719067059386dd329bd5235
Julius Alvarado <javascript.uiux@gmail.com>
2019-10-07 02:33:00 -0700

    practicing return from a generator, we use ->getReturn() to get it  after foreach

--- @study-guide/oop/generators.php
+++ @study-guide/oop/generators.php
@@ -7,31 +7,88 @@
  */
 
 
-/**
- * basic generator prac
- *
- * @param int $c
- * @return Generator
- */
-function genOne (int $c): \Generator {
-    for ($i = 0; $i < $c; $i++) {
-         yield $i;
+//basicGeneratorPractice();
+
+function basicGeneratorPractice(): void {
+
+    /**
+     * basic generator prac
+     *
+     * @param int $c
+     * @return Generator
+     */
+    function genOne(int $c): Generator {
+        for ($i = 0; $i < $c; $i++) {
+            yield $i;
+        }
+    }
+
+    $x = 0;
+    foreach (genOne(10) as $item) {
+        $x += $item;
+        echo "\n $item";
     }
 }
 
-$x = 0;
-foreach (genOne(10) as $item) {
-    $x += $item;
-    echo "\n $item";
+//keyValueGeneratorPrac();
+
+function keyValueGeneratorPrac(): void {
+    // yield by key => value
+    function genKeyVal(): Generator {
+        for ($i = 0; $i < 5; ++$i) {
+            yield 'paper_count' => $i;
+        }
+    }
+
+    foreach (genKeyVal() as $key => $val) {
+        echo "\n$key = $val";
+    }
 }
 
+// yield by reference function &fname
+
+// yieldByReferencePractice();
+
+function yieldByReferencePractice() {
+    function &pracByRef(): Generator {
+        $x = 10;
+        yield $x;
+    }
+
+    $y = pracByRef();
+    $y++;
+
+    return $y;
+}
+
+function clientPrint () {
+    return 'piece printed';
+}
+
+function clientMail () {
+    return 'piece mailed';
+}
+
+function clientJobDone ($status) {
+    return 'piece has been printed and mailed';
+}
+
+function clientGenerator (): Generator {
+    $print = yield clientPrint();
+    $mail = yield clientMail();
+    return clientJobDone($mail);
+}
+
+$gen = clientGenerator();
+
+foreach ($gen as $key => $value) {
+    echo "\n$key, $value\n";
+}
+
+echo $gen->getReturn();
 
 
 
-
-
-
-
-
+$break = 'point';
 
 //
