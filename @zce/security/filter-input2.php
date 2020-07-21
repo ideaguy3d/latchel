98be8b42d53caec7657b437d6c948a7caf19a7ed
julius <phpninja@mail.com>
2020-07-21 14:13:44 -0700

    refactered iterative loop to recursive loop

--- @zce/security/filter-input2.php
+++ @zce/security/filter-input2.php
@@ -1,18 +1,32 @@
 <?php declare(strict_types=1);
 require('./SweetsCompleteDB.php');
 
-$db = new \julius\SweetsCompleteDB();
+$db = (new \julius\SweetsCompleteDB())->pdo;
+
 
 $stateLookupQuery = 'select * from us_states where state_name = :state limit 1;';
-$stateLookupStatement = $db->pdo->prepare($stateLookupQuery);
+$stateLookupStatement = $db->prepare($stateLookupQuery);
 
-$output = '<table border="1">' . PHP_EOL;
-$prospects = $db->pdo->query('select * from prospects limit 99;');
+$output = '<h1>recursive loop</h1> <table border="1">' . PHP_EOL;
+$prospects = $db->query('select * from prospects limit 19;');
+prospectsRecursiveLoop($prospects->fetch());
 
-while($row = $prospects->fetch()) {
+// 1st loop does no validation or sanitation
+//while($row = $prospects->fetch()) {
+//    $output .= '<tr><td>';
+//    $output .= implode('</td><td>', $row);
+//    $output .= '</td></tr>' . PHP_EOL;
+//}
+
+function prospectsRecursiveLoop (array $row): void {
+    global $output;
+    global $prospects;
+    
     $output .= '<tr><td>';
     $output .= implode('</td><td>', $row);
     $output .= '</td></tr>' . PHP_EOL;
+    
+    if($row = $prospects->fetch()) prospectsRecursiveLoop($row);
 }
 
 $output .= '</table>' . PHP_EOL;
