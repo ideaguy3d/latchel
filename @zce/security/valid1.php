82ead7f2368067106df17319a37ff9c9dc87b9dc
julius <phpninja@mail.com>
2020-07-21 20:30:13 -0700

    all fields are getting validated and rendering correctly

--- @zce/security/valid1.php
+++ @zce/security/valid1.php
@@ -13,7 +13,7 @@ else {
     $csvPath = 'test_address_data.csv';
 }
 
-// uti,lity lambda functions
+// utility lambda functions
 $validData = fn($f = '') => '<br /><b style="color:green;">VALID</b>' . $f;
 $invalidData = fn($msg) => '<br /><b style="color:red;">INVALID: ' . $msg . '</b>';
 // remove spaces & dots
@@ -24,10 +24,7 @@ $rSpace = fn($f) => str_replace(' ', '', $f);
 $rSpaceDash = fn($f) => str_replace([' ', '-'], '', $f);
 // remove dash's and parenthesis
 $rDashParen = fn($f) => str_replace(['-', '(', ')'], '', $f);
-
-// _ to represent /
-$_tdtd = '</td><td>';
-
+// escape fields before rendering in html
 $escapeOutput = function(string &$output, array $row): void {
     $output .= '<tr>';
     foreach($row as $field) {
@@ -38,6 +35,13 @@ $escapeOutput = function(string &$output, array $row): void {
 
 $output = '<table border=1>';
 $csv = new SplFileObject($csvPath, 'r');
+
+// add the header row
+$labels = $csv->fgetcsv();
+$output .= '<tr><th>' . implode('</th><th>', $labels) . '</th></tr>' . PHP_EOL;
+
+// '_' to represent '/'
+$_tdtd = '</td><td>';
 
 // store index's for each field
 $idx = new class() {
@@ -57,8 +61,8 @@ $msg = new class() {
     public function __construct() {
         $this->invalid = new class() {
             public string $name = 'Name can only have letters, spaces, and dots';
-            public string $addr = 'Can only include letters, numbers and spaces';
-            public string $postcode = 'Can only include letters, numbers, spaces or dashes';
+            public string $addr = 'Address can only include letters, numbers and spaces';
+            public string $postcode = 'Postcode can only include letters, numbers, spaces or dashes';
             public string $phone = 'Phone numbers can only be numbers, dashes or parenthesis';
             public string $country = 'Country code must only be 2 letters';
             public string $notes = '';
@@ -76,25 +80,37 @@ while($row = $csv->fgetcsv()) {
     $cAddr2 = $rSpace($row[$idx->addr2]);
     $cPostcode = $rSpaceDash($row[$idx->postcode]);
     $cPhone = $rDashParen($row[$idx->phone]);
+    $cCountry = $row[$idx->country];
     
+    // render raw data
     $escapeOutput($output, $row);
     
     // name validation
-    $output .= ctype_alpha($cName) ? $validData($_tdtd) : $invalidData($msg->invalid->name);
+    $output .= ctype_alpha($cName) ? $validData() : $invalidData($msg->invalid->name);
+    $output .= $_tdtd;
     
     // addr1 & addr2 validation
-    $output .= ctype_alnum($cAddr1) ? $validData($_tdtd) : $invalidData($msg->invalid->addr);
-    $output .= ctype_alnum($cAddr2) ? $validData($_tdtd) : $invalidData($msg->invalid->addr);
+    $output .= ctype_alnum($cAddr1) ? $validData() : $invalidData($msg->invalid->addr);
+    $output .= $_tdtd;
+    $output .= ctype_alnum($cAddr2) ? $validData() : $invalidData($msg->invalid->addr);
+    $output .= $_tdtd;
     
     // postcode validation
-    $output .= ctype_alnum($cPostcode) ? $validData($_tdtd) : $invalidData($msg->invalid->postcode);
+    $output .= ctype_alnum($cPostcode) ? $validData() : $invalidData($msg->invalid->postcode);
+    $output .= $_tdtd;
     
     // phone validation
-    $output .= ctype_digit($cPhone) ? $validData($_tdtd) : $invalidData($msg->invalid->phone);
+    $output .= ctype_digit($cPhone) ? $validData() : $invalidData($msg->invalid->phone);
+    $output .= $_tdtd;
     
+    // country code validation
+    $output .= (ctype_alpha($cCountry) && 2 === strlen($cCountry))
+        ? $validData() : $invalidData($msg->invalid->country);
+    $output .= $_tdtd . '<td>&nbsp;</td>';
     
+    $output .= '</tr>' . PHP_EOL;
 }
-
+$output .= '</table>' . PHP_EOL;
 
 ?>
 
