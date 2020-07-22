dd18cbb3704bf11cc5e1db72d52fa26e3618dcfd
julius <phpninja@mail.com>
2020-07-21 19:07:03 -0700

    address2, postalcode, and phone fields validated

--- @zce/security/valid1.php
+++ @zce/security/valid1.php
@@ -14,34 +14,43 @@ else {
 }
 
 // uti,lity lambda functions
-$validData = fn() => '<br /><b style="color:green;">VALID</b>';
+$validData = fn($f = '') => '<br /><b style="color:green;">VALID</b>' . $f;
 $invalidData = fn($msg) => '<br /><b style="color:red;">INVALID: ' . $msg . '</b>';
 // remove spaces & dots
 $rSpaceDot = fn($f) => str_replace([' ', '.'], '', $f);
 // removes spaces
 $rSpace = fn($f) => str_replace(' ', '', $f);
+// remove spaces and dash's
+$rSpaceDash = fn($f) => str_replace([' ', '-'], '', $f);
+// remove dash's and parenthesis
+$rDashParen = fn($f) => str_replace(['-', '(', ')'], '', $f);
+
+// _ to represent /
+$_tdtd = '</td><td>';
 
 $escapeOutput = function(string &$output, array $row): void {
     $output .= '<tr>';
     foreach($row as $field) {
         $output .= '<td>' . htmlspecialchars($field) . '</td>';
     }
-    $output .= '</tr><tr>';
-};
-
-$tdAppend = function() use (&$output) {
-    $output .= '</td><td>';
+    $output .= '</tr><tr><td>';
 };
 
 $output = '<table border=1>';
 $csv = new SplFileObject($csvPath, 'r');
+
 // store index's for each field
 $idx = new class() {
     public int $name = 0;
     public int $addr1 = 1;
     public int $addr2 = 2;
-    public int $zip = 3;
+    public int $postcode = 3;
+    public int $phone = 4;
+    public int $country = 5;
+    public int $notes = 6;
 };
+
+// group messages
 $msg = new class() {
     public object $invalid;
     
@@ -50,10 +59,14 @@ $msg = new class() {
             public string $name = 'Name can only have letters, spaces, and dots';
             public string $addr = 'Can only include letters, numbers and spaces';
             public string $postcode = 'Can only include letters, numbers, spaces or dashes';
+            public string $phone = 'Phone numbers can only be numbers, dashes or parenthesis';
+            public string $country = 'Country code must only be 2 letters';
+            public string $notes = '';
         };
     }
 };
 
+// core validation loop
 while($row = $csv->fgetcsv()) {
     // make sure the current $row is good data
     if(!(isset($row[1]) && is_array($row) && count($row) === 7)) continue;
@@ -61,17 +74,25 @@ while($row = $csv->fgetcsv()) {
     $cName = $rSpaceDot($row[$idx->name]);
     $cAddr1 = $rSpace($row[$idx->addr1]);
     $cAddr2 = $rSpace($row[$idx->addr2]);
+    $cPostcode = $rSpaceDash($row[$idx->postcode]);
+    $cPhone = $rDashParen($row[$idx->phone]);
     
     $escapeOutput($output, $row);
     
     // name validation
-    $output .= '<td>';
-    $output .= ctype_alpha($cName) ? $validData() : $invalidData($msg->invalid->name);
-    $tdAppend(); 
+    $output .= ctype_alpha($cName) ? $validData($_tdtd) : $invalidData($msg->invalid->name);
     
     // addr1 & addr2 validation
-    $output .= ctype_alnum($cAddr1) ? $validData() : $invalidData($msg->invalid->addr);
-    $tdAppend();
+    $output .= ctype_alnum($cAddr1) ? $validData($_tdtd) : $invalidData($msg->invalid->addr);
+    $output .= ctype_alnum($cAddr2) ? $validData($_tdtd) : $invalidData($msg->invalid->addr);
+    
+    // postcode validation
+    $output .= ctype_alnum($cPostcode) ? $validData($_tdtd) : $invalidData($msg->invalid->postcode);
+    
+    // phone validation
+    $output .= ctype_digit($cPhone) ? $validData($_tdtd) : $invalidData($msg->invalid->phone);
+    
+    
 }
 
 
