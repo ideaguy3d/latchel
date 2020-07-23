ca8147efbf069170723482dc2a0d85ff071c5468
julius <phpninja@mail.com>
2020-07-22 23:37:19 -0700

    added an htmlspecialchars() recursion function as well

--- @zce/security/escape1.php
+++ @zce/security/escape1.php
@@ -8,14 +8,25 @@ $testData = [
     "Test string with javascript <script>alert('XSS Attack');</script>"
 ];
 
-echo dataRecursion($testData);
+echo entityRecursion($testData);
+echo specialcharRecursion($testData);
 
-function dataRecursion (array $data): string {
-    static $html = '<ul>';
+function entityRecursion (array $data): string {
+    static $html = '<h2>Using htmlentities():</h2><ul>';
     
-    $item = htmlentities(array_shift($data), ENT_QUOTES, 'UTF-8');
+    $item = htmlentities(array_shift($data), ENT_QUOTES);
     $html .= "<li>escaped output: $item</li>" . PHP_EOL;
     
-    if(!empty($data)) return dataRecursion($data);
-    else return ($html .= '</ul>');
+    if(!empty($data)) return entityRecursion($data);
+    else return ($html .= '</ul><br>');
+}
+
+function specialcharRecursion (array $data): string {
+    static $html = '<h2>Now using htmlspecialchars()</h2><ul>';
+    
+    $elem = htmlspecialchars(array_shift($data), ENT_QUOTES);
+    $html .= "<li>escaped output: $elem</li>" . PHP_EOL;
+    
+    if(!empty($data)) return specialcharRecursion($data);
+    else return ($html .= '</ul><br>');
 }
