327d72754b9f2a179ad63724ef16cb7af40ab258
julius <phpninja@mail.com>
2020-07-23 22:30:21 -0700

    using DateInterval to do datetime calculations ❤😍😊

--- @zce/data-formats/datetime.php
+++ @zce/data-formats/datetime.php
@@ -13,6 +13,33 @@ $dates = [
 ];
 
 try {
+    // practicing date calculations with DateInterval
+    //DateTime::createFromFormat('m-d-Y H:i:s', '01-14-2021 14:24:15');
+    $dateTime = new DateTime();
+    $curTime = $dateTime->format(DateTime::COOKIE);
+    // SUPER COOL !!
+    // 'P1Y5M21DT9H38M11S' = 1year 5months 21days 9hours 38mins 11secs
+    $interval2 = 'P1Y1M2DT9H38M11S';
+    $interval = 'P1M1DT1H2M4S';
+    $dateInterval = new DateInterval($interval);
+    $futureTime = $dateTime->add($dateInterval);
+    $futureTime = $dateTime->format(DateTime::COOKIE);
+    $dateInterval2 = new DateInterval($interval2);
+    $futureTime2 = $dateTime->add($dateInterval2);
+    $futureTime2 = $dateTime->format(DateTime::COOKIE);
+    echo "\n\n1_> $curTime";
+    echo "\n\n2_> $futureTime";
+    echo "\n\n3_> $futureTime2";
+    /*
+        //-- output:
+        1_> Friday, 24-Jul-2020 07:27:58 CEST
+        2_> Tuesday, 25-Aug-2020 08:30:02 CEST
+        3_> Monday, 27-Sep-2021 18:08:13 CEST
+    */
+    
+    
+    echo "\n\n";
+    
     // to avoid confusion between Aug 9th 2020 between "9-8-20" & "8-9-20"
     $date = '8-9-20';
     // month = Aug
