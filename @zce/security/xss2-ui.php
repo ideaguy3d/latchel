9ed4926b62f7c2439c49934b68dfa3aa8c4ee1be
julius <phpninja@mail.com>
2020-07-26 15:58:01 -0700

    added login, a hash with session to prevent form hijacking, and
    better comments to help refresh my memory

--- @zce/security/xss2-ui.php
+++ @zce/security/xss2-ui.php
@@ -1,12 +1,19 @@
-<?php
+<?php /*
+~ Protecting against XSS attacks(stored/reflected), session hijacking, session fixation ~
 
-/*
-~ protecting against stored XSS attacks ~
-protect_against_xss_stored_1
-protect_against_xss_stored_2
-*/
+1. Filter, Validate, Escape, & Sanitize (helps protect against XSS):
+    htmlspecialchars(), htmlentities(), preg_replace(), str_replace(), filter_var()
+    ctype_*(), preg_match(), strlen(), strip_tags(), casting to correct data type,
 
-// prevent PHP from echoing out information
+2. Session Hijacking:
+    session_regenerate_id(), One time Hash(e.g. md5()) for $_SESSION, timed logout,
+    create logout(
+
+3. Session Fixation: */
+
+session_start(); //session_regenerate_id();
+
+// script vars, don't store important info
 $id = 0;
 $name = '';
 $image = '';
@@ -22,7 +29,7 @@ $default = new class() {
     public int $imageMax = 128;
     public int $valid = 0;
     public int $maxId = 999999;
-    public array $error = ['id' => false, 'name' => false, 'image' => false];
+    public array $error = ['id' => false, 'name' => false, 'image' => false, 'hash' => false];
     public string $validElem = '<p class="valid">Valid Input ✅😎👌</p>';
     
     public function error(string $input): string {
@@ -31,6 +38,15 @@ $default = new class() {
 };
 
 // All inputs should be tested if they are set, and then properly filtered
+$username = $_GET['username'] ?? false;
+if($username) {
+    $username = strip_tags($username);
+    $_SESSION['username'] = $username;
+}
+else if(isset($_SESSION['name'])) {
+    $username = $_SESSION['username'];
+}
+
 $id = $_GET['id'] ?? null;
 $id = !is_null($id) ? (int)$id : 0; // casting to int can filter xss
 
@@ -61,19 +77,36 @@ else if(!preg_match('/^.+(jpg|png)$/i', $image)) {
 }
 else $default->valid++;
 
+// Use a hash(e.g. md5()) & $_SESSION to prevent automated systems from
+// hijacking the form(re-creating it) & auto-submitting it repeatedly
+if($_SESSION['hash'] ?? null) {
+    if(isset($_POST['hash']) && $_POST['hash'] == $_SESSION['hash']) $default->valid++;
+    else $default->error['hash'] = 'ERROR: Please re-submit form';
+}
+$_SESSION['hash'] = md5($_SERVER['REMOTE_ADDR'] . date('m-d-Y H:i:s'));
+
+//TODO:
+// - create a CAPTCHA,
+// - use HTML5 built in client validation
+
+// log user out and destroy session
+
+
 // to help me debug
-echo "id = $id, name = $name, image = $image";
+echo "<p class='debug-info'>id = $id, name = $name, image = $image, username = $username</p><hr>";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- setting the proper charset can help if an attacker used a foreign character set -->
     <meta charset="UTF-8">
     <title>Protect Against Stored XSS</title>
     <style>
         html {
             font-family: sans-serif;
         }
+
         table {
             width: 600px;
         }
@@ -86,78 +119,108 @@ echo "id = $id, name = $name, image = $image";
         td {
             border: thin solid black;
         }
-        
+
+        .debug-info {
+            font-family: Consolas;
+            font-size: small;
+        }
+
         .valid {
             color: #1dc116;
             padding: 4px;
+        }
+        .float-right {
+            float: right;
+            padding-right: 16px;
         }
     </style>
 </head>
 
 <body>
-<h1>protect_against_xss_stored_1.php</h1>
-<h2>To Notice:</h2>
-<ul>
-    <li>First time: <i>Undefined Index</i> notices</li>
-    <li>All fields are subject to stored XSS</li>
-</ul>
 
-<br/>Try entering the following values:
-<table>
-    <tr>
-        <th>Field</th>
-        <th>Value</th>
-    </tr>
-    <tr>
-        <td><i>id</i>
-        <td>
-            <pre>&lt;script>alert('XSS ATTACK');&lt;/script></pre>
-        </td>
-    </tr>
-    <tr>
-        <td><i>name</i></td>
-        <td>
-            <pre>%26%23x3C%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B%26%23x61%3B%26%23x6C%3B%26%23x65%3B%26%23x72%3B%26%23x74%3B%26%23x28%3B%26%23x27%3B%26%23x58%3B%26%23x53%3B%26%23x53%3B%26%23x20%3B%26%23x41%3B%26%23x54%3B%26%23x54%3B%26%23x41%3B%26%23x43%3B%26%23x4B%3B%26%23x27%3B%26%23x29%3B%26%23x3B%3B%26%23x3C%3B%26%23x2F%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B</pre>
-            NOTE: urldecode() shows the value
-            as <?php echo urldecode('%26%23x3C%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B%26%23x61%3B%26%23x6C%3B%26%23x65%3B%26%23x72%3B%26%23x74%3B%26%23x28%3B%26%23x27%3B%26%23x58%3B%26%23x53%3B%26%23x53%3B%26%23x20%3B%26%23x41%3B%26%23x54%3B%26%23x54%3B%26%23x41%3B%26%23x43%3B%26%23x4B%3B%26%23x27%3B%26%23x29%3B%26%23x3B%3B%26%23x3C%3B%26%23x2F%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B'); ?>
-        </td>
-    </tr>
-    <tr>
-        <td><i>image</i></td>
-        <td>
-            <pre>http://localhost:60/xss.php</pre>
-        </td>
-    </tr>
-</table>
+<p class="float-right">
+    Welcome <b><?= $username ? htmlentities($username) : 'Guest' ?></b>
+    <br> id: <small> <?= session_id() ?></small>
+</p>
 
-<br/>
+<!-- Login -->
+<?php if(false === $username) { ?>
+    <form>
+        <p>Please login:</p>
+        <input name="username" type="text" maxlength="128" placeholder="username"><br>
+        <input type="submit" value="Login"><br>
+    </form>
+<?php } ?>
 
-<!-- 3 input fields were checked -->
-<p><?= 3 === $default->valid ? 'Successfully saved data' : 'Error, unable to save data' ?></p>
-<form>
+<?php if(!empty($username)) { ?>
     <table>
-        <tr class="form-id">
-            <th>ID</th>
-            <td><input type="text" name="id" size="8" maxlength="8"/></td>
-            <!-- htmlentities($id);  -->
-            <td>Current Value: <?php echo $id ?></td>
-            <td><?= $default->error['id'] ?></td>
+        <tr>
+            <th>Field</th>
+            <th>Value</th>
         </tr>
-        <tr class="form-name">
-            <th>Name</th>
-            <td><input type="text" name="name" maxlength="128"/></td>
-            <td>Current Value: <?php echo htmlentities($name); ?></td>
-            <td><?= $default->error('name') ?></td>
+        <tr>
+            <td><i>id</i>
+            <td>
+                <pre>&lt;script>alert('XSS ATTACK');&lt;/script></pre>
+            </td>
         </tr>
-        <tr class="form-image">
-            <th>Image</th>
-            <td><input type="text" name="image"/></td>
-            <td>Current Value: <img src="<?php echo htmlentities($image); ?>"/></td>
-            <td><?= $default->error['image'] ?></td>
+        <tr>
+            <td><i>name</i></td>
+            <td>
+                <pre>%26%23x3C%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B%26%23x61%3B%26%23x6C%3B%26%23x65%3B%26%23x72%3B%26%23x74%3B%26%23x28%3B%26%23x27%3B%26%23x58%3B%26%23x53%3B%26%23x53%3B%26%23x20%3B%26%23x41%3B%26%23x54%3B%26%23x54%3B%26%23x41%3B%26%23x43%3B%26%23x4B%3B%26%23x27%3B%26%23x29%3B%26%23x3B%3B%26%23x3C%3B%26%23x2F%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B</pre>
+                NOTE: urldecode() shows the value
+                as <?php echo urldecode('%26%23x3C%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B%26%23x61%3B%26%23x6C%3B%26%23x65%3B%26%23x72%3B%26%23x74%3B%26%23x28%3B%26%23x27%3B%26%23x58%3B%26%23x53%3B%26%23x53%3B%26%23x20%3B%26%23x41%3B%26%23x54%3B%26%23x54%3B%26%23x41%3B%26%23x43%3B%26%23x4B%3B%26%23x27%3B%26%23x29%3B%26%23x3B%3B%26%23x3C%3B%26%23x2F%3B%26%23x73%3B%26%23x63%3B%26%23x72%3B%26%23x69%3B%26%23x70%3B%26%23x74%3B%26%23x3E%3B'); ?>
+            </td>
+        </tr>
+        <tr>
+            <td><i>image</i></td>
+            <td>
+                <pre>http://localhost:60/xss.php</pre>
+            </td>
         </tr>
     </table>
-    <input type="submit"/>
-</form>
+
+    <br/>
+
+    <!-- 3 input fields were checked -->
+    <h1><?= 3 === $default->valid ? 'Successfully saved data' : 'Error, unable to save data' ?></h1>
+    <form>
+        <table>
+            <tr class="form-id">
+                <th>ID</th>
+                <td><input type="text" name="id" size="8" maxlength="8"/></td>
+                <!-- htmlentities($id);  -->
+                <td>Current Value: <?php echo $id ?></td>
+                <td><?= $default->error('name') ?></td>
+            </tr>
+            <tr class="form-name">
+                <th>Name</th>
+                <td><input type="text" name="name" maxlength="128"/></td>
+                <td>Current Value: <?php echo htmlentities($name); ?></td>
+                <td><?= $default->error('name') ?></td>
+            </tr>
+            <tr class="form-image">
+                <th>Image</th>
+                <td><input type="text" name="image"/></td>
+                <td>
+                    Current Value: <img src="<?php echo htmlentities($image); ?>"/>
+                    <br> <small>img url: "<?= htmlentities($image) ?>"</small>
+                </td>
+                <td><?= $default->error('name') ?></td>
+            </tr>
+        </table>
+        <input type="hidden" value="<?= $_SESSION['hash'] ?>">
+        <input type="hidden" value="<?= $username ?: '' ?>">
+        <br>
+        <input type="submit"/>
+    </form>
+
+<?php } else { ?>
+
+    <p>Unautheticated User 🛑</p>
+
+<?php } ?>
+
 
 </body>
 </html>
