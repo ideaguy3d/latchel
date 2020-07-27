0baae6a6c6d4ed140ca18d6f4748fe365f75a29a
julius <phpninja@mail.com>
2020-07-26 23:53:05 -0700

    made cart, login, and index more secure

--- +sweetsecure/View/cart.php
+++ +sweetsecure/View/cart.php
@@ -12,25 +12,26 @@ if(isset($_POST['checkout']) && isset($_SESSION['cart'])) {
 else if(isset($_POST['back'])) {
     header('Location: ?page=products');
     exit;
-    
-    
 }
 // process removals / updates
 else if(isset($_POST['change'])) {
     if(isset($_POST['remove'])) {
         foreach($_POST['remove'] as $key => $id) {
             // *** filtering: need to filter the $id
-            $productTable->delProductFromCart($id);
+            $productTable->delProductFromCart((int)$id);
         }
     }
     if(isset($_POST['update'])) {
         foreach($_POST['update'] as $key => $id) {
             // *** filtering: force to int
             // *** error handling: provide default if not set
-            $qty = $_POST['qty'][$key];
-            $notes = $_POST['notes'][$key];
+            $qty = isset($_POST['qty'][$key]) ? (int)$_POST['qty'][$key] : 0;
+            $notes = isset($_POST['notes'][$key]) ? strip_tags($_POST['notes'][$key]) : '';
+            
             // *** error handling: check to make sure $qty and $id are set
-            $productTable->updateProductInCart($id, $qty, $notes);
+            if($id && $qty && $notes) {
+                $productTable->updateProductInCart($id, $qty, $notes);
+            }
         }
     }
 }
@@ -60,25 +61,27 @@ $cart = $productTable->getShoppingCart(session_id());
                 $key = 0;
                 foreach($cart as $item) {
                     // filtering: force to int or float
-                    $total += $item['qty'] * $item['price'];
+                    $total += floatval($item['qty'] * $item['price']);
                     $link = '?page=detail&id=' . $item['product_id']; ?>
                     <tr>
                         <td><?= $item['sku']; ?></td>
-                        <td><a href="<?= $link; ?>">
+                        <td>
+                            <a href="<?= $link; ?>">
                                 <img src="images/<?= $item['link']; ?>.scale_20.JPG"
                                      alt="<?= $item['title']; ?>" width="60" height="60"/>
                             </a>
                         </td>
-                        <td><?= $item['title']; ?>
+                        <td>
+                            <?= $item['title']; ?>
                             <!-- // *** output escaping needed! -->
-                            <br/>Notes: <?= $item['notes']; ?>
+                            <br/>Notes: <?= htmlentities($item['notes']) ?>
                             <br/>
                             <!-- Susceptible _XSS: "<script>alert(4444)</script>" -->
                             <input type="text" name="notes[<?= $key; ?>]" value="xNotes" class="s0" size="40"/>
                         </td>
                         <!-- // *** output escaping needed! -->
-                        <td>Qty: <br/><input type="text" value="<?php echo $item['qty']; ?>"
-                                             name="qty[<?php echo $key; ?>]" class="s0" size="2"/></td>
+                        <td>Qty: <br/><input type="text" value="<?php echo htmlentities((int)$item['qty']) ?>"
+                                             name="qty[<?php echo $key ?>]" class="s0" size="2"/></td>
                         <td align="right"><?php printf('%8.2f', $item['price']); ?></td>
                         <td align="right"><?php printf('%8.2f', $item['qty'] * $item['price']); ?></td>
                         <td>
--- +sweetsecure/View/login.php
+++ +sweetsecure/View/login.php
@@ -4,51 +4,86 @@ require './Model/Members.php';
 $memberTable = new Members();
 
 // check to see if login
-if (isset($_POST['data'])) {
-	// *** take security precautions: filter all incoming data!
-	$email 		= $_POST['data']['email'];
-	$password 	= $_POST['data']['password'];
-	if ($email && $password) {
-		$result = $memberTable->loginByName($email, $password);
-		// *** session hijacking protection: implement 1 way hash & CAPTCHA
-		if ($result) {
-			// store user info in session
-			$_SESSION['user'] = $result;
-			$_SESSION['login'] = TRUE;
-		} else {
-			$_SESSION['login'] = FALSE;
-		}
-		// redirect back home
-		header('Location: ?page=home');
-		exit;
-	}
-}
+if(isset($_POST['data'])) {
+    $email = $_POST['data']['email'] ?? '';
+    $password = $_POST['data']['password'] ?? '';
+    $phrase = $_POST['data']['phrase'] ?? '';
+    $hash = $_POST['data']['hash'] ?? '';
+    
+    // *** take security precautions: filter all incoming data!
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    $password = strip_tags(trim($password));
+    $phrase = strip_tags(trim($phrase));
+    
+    if($email && $password && $phrase && $hash) {
+        // *** session hijacking protection: implement 1 way hash & CAPTCHA
+        if(!(isset($_SESSION['hash']) && $_SESSION['hash'] == $hash)) {
+            $valid = false;
+            //TODO: log this
+            $error [] =  'Invalid form submission, please re-submit';
+        }
+        
+        $result = $memberTable->loginByName($email, $password);
+        if($result) {
+            // store user info in session
+            $_SESSION['user'] = $result;
+        }
+        else {
+            $valid = false;
+            $error [] = 'Unable to login';
+        }
+    }
+    else {
+        $valid = false;
+        $error [] = 'There is missing info';
+    }
+    
+    if($valid) {
+        $_SESSION['login'] = true;
+        header('Location: ?page=home');
+        exit;
+    }
+    else {
+        $_SESSION['login'] = false;
+    }
+    
+} // END OF: if(isset($data)){}
+
+$newHash = md5(date('YmdHis') . session_id());
+$_SESSION['hash'] = $newHash;
 
 ?>
+
 <div class="content">
-	<br/>
-	<div class="product-list">
+    <br/>
+    <div class="product-list">
 
-		<h2>Login</h2>
-		<br/>
+        <h2>Login</h2>
+        <br/>
 
-		<b>Please enter your information.</b><br/><br/>
-		<form action="?page=login" method="POST">
-			<p>
-				<label>Email: </label>
-				<!-- // *** consider using the HTML5 "email" type instead of "text" -->
-				<input type="text" name="data[email]" />
-			<p>
-			<p>
-				<label>Password: </label>
-				<!-- // *** consider using the "password" type instead of "text" -->
-				<input type="text" name="data[password]" />
-			<p>
-			<p>
-				<!-- // *** session hijacking prevention: add 1 way hash + CAPTCHA -->
-				<input type="reset" name="data[clear]" value="Clear" class="button"/>
-				<input type="submit" name="data[submit]" value="Submit" class="button marL10"/>
-			<p>
-		</form>
-	</div><!-- product-list -->
+        <b>Please enter your information.</b><br/><br/>
+        <form action="?page=login" method="POST">
+            <p>
+                <label>Email: </label>
+                <!-- // *** consider using the HTML5 "email" type instead of "text" -->
+                <input type="email" name="data[email]"/>
+            </p>
+            <p>
+                <label>Password: </label>
+                <!-- // *** consider using the "password" type instead of "text" -->
+                <input type="text" name="data[password]"/>
+            </p>
+            <p>
+                <!-- // *** session hijacking prevention: add 1 way hash + CAPTCHA -->
+                <input type="reset" name="data[clear]" value="Clear" class="button"/>
+                <input type="submit" name="data[submit]" value="Submit" class="button marL10"/>
+                <input type="hidden" name="data[hash]" value="<?= $newHash ?>">
+            </p>
+            <p style="color: red">
+                <?php if(!$valid): ?>
+                    <?= implode('<br>', $error) ?>
+                <?php endif; ?>
+            </p>
+        </form>
+    </div><!-- product-list -->
 </div>
