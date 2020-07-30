4c54e08f9886a9f6df80a4e0bcd78ac435b8e6b4
julius <phpninja@mail.com>
2020-07-29 22:23:54 -0700

    "yield from" generator delegation

---
+++ @zce/oop/gen3.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 7/29/2020
+ * Time: 9:52 PM
+ */
+
+use oop\models\ModelOne;
+
+const GEN_LIMIT = 4;
+
+$percep = per();
+
+foreach($percep as $metric) {
+    echo "\n\: $metric\n";
+}
+
+function per() {
+    $types = ['classify', 'predict', 'recommend'];
+    $fastArr = SplFixedArray::fromArray($types);
+    
+    $struct = new class(){
+        public string $x = 'Hello';
+        public string $y = 'World';
+        public int $z = GEN_LIMIT * 4;
+    };
+    
+    yield from $fastArr;
+    $archetype = new ModelOne($fastArr[2]);
+    yield from (array)$struct;
+    yield from (array)$archetype;
+    yield from cep();
+    $archetype->update();
+}
+
+function cep() {
+    for($i = 0; $i < GEN_LIMIT; $i++) {
+        yield 'test' => $i;
+    }
+}
--- @zce/oop/models/ModelOne.php
+++ @zce/oop/models/ModelOne.php
@@ -7,6 +7,8 @@ namespace oop\models;
 class ModelOne
 {
     public string $modelType;
+    public string $mode = 'quick';
+    public int $reduceTo = 2;
     
     public function __construct($regression) {
         echo "ModelOne is being constructed!";
