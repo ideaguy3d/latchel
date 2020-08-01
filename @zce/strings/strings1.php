25641b1f68cdfcd9c18570cd1ee5eda4e2256dad
julius <phpninja@mail.com>
2020-07-31 18:33:14 -0700

    multibyte strings

--- @zce/strings/strings1.php
+++ @zce/strings/strings1.php
@@ -9,9 +9,50 @@
 
 // encoding / multi byte / char output prac
 
-newOffset();
+//newOffset();
+echo basic2();
 
-function newOffset () {
+
+function basic1() {
+    $start = hrtime();
+    $per = "\u{1F418}"; // a cute lil elephpant 🐘
+    $dim = file_get_contents('me.png');
+    $cep = 'hello solar system';
+    $end = hrtime()[1] / $start[1];
+    
+    $mb_dim = mb_detect_encoding($dim);
+    $mb_cep = mb_detect_encoding($cep);
+    $mb_per = mb_detect_encoding($per);
+    
+    $debug = 1;
+}
+
+function basic2() {
+    static $ways = [
+        'emoji' => "\u{1F44B}",
+        'latinChars' => "Hello",
+        'accentedChars' => "ça va?",
+    ];
+    
+    $method = array_key_first($ways);
+    $enc = array_shift($ways);
+    $encode = mb_detect_encoding($enc, ['ASCII', 'UTF-8', 'ISO-8859-1']);
+    $strLen = strlen($enc);
+    $strMbLen = mb_strlen($enc);
+    $firstChar = $enc[0];
+    
+    if(!empty($ways)) {
+       basic2(); // return
+    }
+
+    $r = "\n -$method: $enc \n -$encode \n -len: $strLen, $strMbLen \n -char1: $firstChar\n";
+    $debug = "$r[0], $r[5]";
+    
+    // **Recurse_Point** \\
+    return $r;
+}
+
+function newOffset() {
     $v = new StrArrayAccess('Perceptron');
     
     // now it works
@@ -21,7 +62,7 @@ function newOffset () {
     unset($v[-1]);
 }
 
-function oldOffset () {
+function oldOffset() {
     $v = 'Perceptron';
     $v = substr_replace($v, '', -1, 1);
     
