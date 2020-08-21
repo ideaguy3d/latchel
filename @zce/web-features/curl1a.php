ca20798adc87159a9fb99e66f86e2d0150837da6
julius <phpninja@mail.com>
2020-08-21 13:39:01 -0700

    some client url practice

---
+++ @zce/web-features/curl1a.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 8/20/2020
+ * Time: 6:19 PM
+ *
+ * curl functions used:
+ * 1. curl_init()
+ * 2. curl_setopt()
+ * 3. curl_error()
+ * 4. curl_exec()
+ * 5. curl_close()
+ *
+ */
+
+$resource = "169.254.241.146/+sweetsecure/curl1b.php";
+
+$curlResource = curl_init($resource);
+
+$curlData = ['foo' => 'bar'];
+
+curl_setopt($curlResource, CURLOPT_POST, true);
+
+curl_setopt($curlResource, CURLOPT_POSTFIELDS, $curlData);
+
+$response = curl_exec($curlResource);
+
+$err = curl_error($curlResource);
+
+curl_close($curlResource);
+
+$log = "\n|\n|\n_> application response = $response\n\n";
+
+echo $log;
