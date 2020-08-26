72ff7de468dc5d3a052553eeab8a5e428ddc8412
julius <phpninja@mail.com>
2020-08-26 02:52:42 -0700

    using DOMDocument class to create some XML files

--- @zce/data-formats/dom1.php
+++ @zce/data-formats/dom1.php
@@ -7,40 +7,75 @@
  *
  **** Functions used ****
  * new DomDocument()
- * appendChild()
- * createElement()
+ * new DomElement()
+ * ->appendChild()
+ * ->createElement()
+ * ->formatOutput = true;
+ * ->saveXML()
  */
 
-(function() {
+header('Content-Type: application/xml');
+
+$cdList = [
+    ['title' => 'Trance', 'artist' => 'DJ Gogo', 'year' => 2015],
+    ['title' => 'Techno', 'artist' => 'TiestoX', 'year' => 2019],
+    ['title' => 'Big Room House', 'artist' => 'Electro1', 'year' => 2019],
+    ['title' => 'Happy Hardcore', 'artist' => 'DJ Cotts', 'year' => 2017],
+];
+
+xmlTwo($cdList);
+
+function xmlOne () {
     try {
         // root
         $dom = new DOMDocument('1.0', 'UTF-8');
         $root = $dom->appendChild($dom->createElement('Movies'));
-    
+        
         // child elem's
         $movie = new DOMElement('movie');
         $title = new DOMElement('title', 'A History of');
-    
+        
         $root->appendChild($movie)->appendChild($title);
-    
+        
         $movie->appendChild(new DOMElement('year', '2005'));
         $movie->appendChild(new DOMElement('country', 'USA'));
         $movie->appendChild(new DOMElement('genre', 'crime'));
-    
+        
         $director = new DOMElement('director');
         $movie->appendChild($director);
-    
+        
         $director->appendChild(new DOMElement('first_name', 'Joe'));
         $director->appendChild(new DOMElement('last_name', 'Shmoe'));
         $director->appendChild(new DOMElement('birth_date', 1970));
-    
+        
         // root, format&output
         $dom->formatOutput = true;
         $xmlOutput = $dom->saveXML();
-        file_put_contents('movies.xml', $xmlOutput);
+        
+        //file_put_contents('movies.xml', $xmlOutput);
+        echo $xmlOutput;
     }
     catch(Throwable $e) {
         echo $e->getMessage();
     }
-})();
+}
 
+function xmlTwo ($cds) {
+    $dom = new DOMDocument('1.0','UTF-8');
+    $collection = $dom->createElement('collection');
+    $dom->appendChild($collection);
+    
+    foreach($cds as $cd) {
+        $cdNode = $dom->createElement('cd');
+        foreach($cd as $name => $value) {
+            $cdNode->appendChild($dom->createElement($name, $value));
+        }
+        $collection->appendChild($cdNode);
+        unset($cdNode);
+    }
+    
+    $dom->formatOutput = true;
+    $output = $dom->saveXML();
+    file_put_contents('cd-list.xml', $output);
+    $debug = 1;
+}
