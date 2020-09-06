3c6993f0144d81ef91578b1bdc27d2ac2f06b6ae
julius <phpninja@mail.com>
2020-09-06 05:17:46 -0700

    Create type-closure.php

---
+++ @zce/functions/type-closure.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 9/6/2020
+ * Time: 5:10 AM
+ */
+
+
+$commission = function($x) {
+    return ($x * 2) + 100;
+};
+
+function salesCom(closure $lambda, $sale) {
+    if($sale > 10000) {
+        return $lambda(0.125);
+    }
+    return $lambda(0.25);
+}
+
+$sale = 4_899;
+echo "\n Sales commission = " . salesCom($commission, $sale);
