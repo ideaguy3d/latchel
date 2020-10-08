7ce5d75aef63866b882d725d0a0daeca83b603fb
julius <phpninja@mail.com>
2020-10-07 18:27:30 -0700

    ->bindColumn() practice

--- @zce/sql/q1.php
+++ @zce/sql/q1.php
@@ -2,14 +2,18 @@
 
 try {
     $pdo = new PDO(
-        'mysql:host=localhost;dbname=hack_match', 'root', '',
-        [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]
+        'mysql:host=localhost;dbname=hack_match', 'root', ''
+      /*  , [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]*/
     );
     
-    $sql = 'select name, pos from test_one where pos < 10';
+    $sql1 = 'select name, pos from test_one where pos < 10';
     $sql = 'select :col from tests_one where name = :name';
     $sql1 = "insert into test_one (name, pos) values ('michelle', 4)";
     $sqlCount = "select count(*) from test_one";
+    $sql = 'select * from test_one';
     
     $prepare = function() use ($pdo, $sql) {
         $statement = $pdo->prepare($sql);
@@ -29,11 +33,25 @@ try {
         $pdo->rollback();
         $statement = $pdo->query($sql2);
         $numRows2 = $statement->fetchColumn();
-        $debug  = 1;
+        $debug = 1;
         echo "\n_> numRows = $numRows, numRows2 = $numRows2\n";
     };
     
-    $transaction($pdo, $sql1, $sqlCount);
+    $bindColumn = function(PDO $pdo, $sql) {
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(1, 'Jane');
+        $statement->bindColumn(1, $pos);
+        $statement->execute();
+        
+        while($row = $statement->fetch(PDO::FETCH_OBJ)) {
+            echo "\n_> item = $pos\n";
+            $debug = 1;
+        }
+        $debug = 1;
+    };
+    
+    $bindColumn($pdo, $sql);
+    //$transaction($pdo, $sql1, $sqlCount);
 }
 catch(PDOException $e) {
     exit ("__>> PDO_EXCEPTION:\n\n" . $e->getMessage());
